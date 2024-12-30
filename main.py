e3df0fa6363b8390606a42d5e3b2ed677a2e815e
Perchik <ilya1880@mail.ru>
2024-12-30 16:52:23 +0300

    Работает "Добавить"(self.add_amount) Думаю как статистику сделать на втором экране

--- main.py
+++ main.py
@@ -12,14 +12,14 @@ from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.carousel import Carousel
 from kivy.properties import ListProperty, StringProperty, NumericProperty,ObjectProperty
 
 from kivy.core.window import Window
 
 from kivy.lang.builder import Builder
-from pygments.styles.dracula import comment
+
 
 """Установить kv файл в директорию совместно в main.py"""
 dir_name = os.path.split(os.path.abspath(__file__))
@@ -55,6 +55,9 @@ class TextInpMoneyKop(TextInput):
             return super().insert_text(enter_symbol, from_undo=from_undo)
 
 class Pages(Carousel):
+    two_pages = ObjectProperty()
+    scroll_two_pages = ObjectProperty()
+
     day_day_spinner = ObjectProperty()
     month_month_spinner = ObjectProperty()
     rubel_money = ObjectProperty()
@@ -74,15 +77,17 @@ class Pages(Carousel):
     input_kopeyka_for_label = StringProperty("")
 
     label_month_lst = ListProperty([CURRENT_DAY, CURRENT_MONTH])  # Устан.даты в Label_amount
-    label_amount_for_day = StringProperty()
+    # label_amount_for_day = StringProperty()
 
     def __init__(self, **kwargs):
         super(Pages, self).__init__(**kwargs)
         self.loop = True  # Бесконечная прокрутка
+
         # self.load_slide(self.next_slide)  ##################### следущий Pages
-        self.KEY_DICT = ""
-        self.VALUE_DICT = ""
-        self.COMMENT_DICT = ""
+
+        # self.KEY_DICT = ""
+        # self.VALUE_DICT = ""
+        # self.COMMENT_DICT = ""
         self.file_dict = {}
         self.main()
 
@@ -90,6 +95,58 @@ class Pages(Carousel):
         self.file_dict = self.load_or_create_file_dict()
         key = self.get_key_for_dict()
         self.update_statistic(key)
+
+
+
+
+
+
+
+
+
+
+
+    def create_label_statistic_two_page(self):
+        self.two_pages.bind(minimum_height=self.two_pages.setter('height'))
+
+        for i in range(1,3):
+            box_data = BoxLayout()
+            lab_data = Label(padding=5,text=f"{i+1} Декабря",size_hint_y=None, height=40,
+                         size_hint_x= .5)
+
+            lab_money = Label(padding=5,text=f"{i}руб {i+100}коп",size_hint_y=None, height=40,
+                         size_hint_x= .5)
+
+            lab_comment = Label(padding=5,text=f"Чай,вода,булочка,пивас,ганджубас",size_hint_y=None, height=40,
+                         size_hint_x= .5)
+            box_data.add_widget(lab_data)
+            box_data.add_widget(lab_money)
+            box_data.add_widget(lab_comment)
+
+        self.two_pages.add_widget(box_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     def update_statistic(self,key:str):
         print(key,"95 Мартобря")
@@ -141,8 +198,8 @@ class Pages(Carousel):
 
     def work_spinner_day_month(self):
         key = self.get_key_for_dict()
-        self.input_rubel = "0"
-        self.input_kopeyka = "0"
+        self.rubel_money.text = "0"
+        self.kopeyka_money.text = "0"
         self.update_statistic(key)
 
 
@@ -158,7 +215,7 @@ class Pages(Carousel):
             comment = self.comment_comment.text
             value = self.create_value_for_dict(self.rubel_money.text,self.kopeyka_money.text,comment)
             self.file_dict[key] = value
-            self.write_file_time_work()
+            self.write_file()
         else:
             self.my_popup_rewrite_add()
 
@@ -170,90 +227,90 @@ class Pages(Carousel):
 
 
     def add_amount(self):
-        old_amount_rub = int(self.get_parser_money(self.file_dict[self.KEY_DICT],rub=True))*100
-        old_amount_kops = int(self.get_parser_money(self.file_dict[self.KEY_DICT],kop=True))
+        key = self.get_key_for_dict()
+        old_amount_rub = int(self.get_parser_money(key,rub=True))*100
+        old_amount_kops = int(self.get_parser_money(key,kop=True))
         total_save_amount_in_kop = old_amount_rub + old_amount_kops
-
 
         current_amount_rub = int(self.rubel_money.text)*100
         current_amount_kops = int(self.kopeyka_money.text)
         total_currents_amount_in_kop = current_amount_rub + current_amount_kops
 
-        total_summ_value_in_dict = (total_save_amount_in_kop + total_currents_amount_in_kop)/100
+        total_summ_value_in_dict = str((total_save_amount_in_kop + total_currents_amount_in_kop)/100)
+        # сформировать новое значение
+        rub = total_summ_value_in_dict.split(".")[0]
+        kop = total_summ_value_in_dict.split(".")[1]
+        comment = self.comment_comment.text
+        new_value = self.create_value_for_dict(rub,kop,comment)
+        self.file_dict[key] = new_value
+        self.write_file()
+        self.rubel_money.text = "0"
+        self.kopeyka_money.text = "0"
+        print("New summ value",total_summ_value_in_dict)
 
-        rub = self.get_parser_money(str(total_summ_value_in_dict),rub=True)
-        kop = self.get_parser_money(str(total_summ_value_in_dict),kop=True)
-        # создать новый ключ:значение и отправить на upgrade()
-
-        print(rub,kop)
-
-        print("New summ value",str(total_summ_value_in_dict))
-        print("Куда-то надо отправить")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def del_amount(self):
+        key = self.get_key_for_dict()
+        if key in self.file_dict:
+            del self.file_dict[key]
+            Clock.schedule_once(self.my_callback, 2)
+            self.label_save_txt = "Дата удалена"
+        else:
+            Clock.schedule_once(self.my_callback, 2)
+            self.label_save_txt = "Такой даты не существует"
 
 
 
     def my_popup_rewrite_add(self):
         def answer_popup(instance):
+
             if instance.text == "Переписать сумму":
                 self.overwriting_values()
                 mynepopup.dismiss()
-            elif instance.text == "Добавить сумму":
+            elif instance.text == "Добавить данные":
                 self.add_amount()
                 mynepopup.dismiss()
-        mynepopup = Popup(title="Info", size_hint=(.8, .4))
-        container = FloatLayout(size_hint=(1, 1))
-        lab = Label(text="Рабочий день на эту дату существует.",
-                    font_size=30, size_hint=(1, .3), pos_hint={'x': .001, 'top': 1},
+            elif instance.text == "Удалить сумму":
+                self.del_amount()
+                mynepopup.dismiss()
+
+
+        mynepopup = Popup(title="Изменение суммы")
+        root_container = BoxLayout(size_hint=(1, 1), orientation="vertical")
+        label_container = BoxLayout(size_hint=(1, .3))
+        lab = Label(text="Сумма на эту дату существует.",
+                    font_size=40, size_hint=(1, .3), pos_hint={'x': .001, 'top': 1},
                     halign='center')
-        container.add_widget(lab)
-        btn_ok = Button(text="Переписать сумму", size_hint=(.3, .3),
-                        pos_hint={'x': .15, 'y': .1})
-        btn_ok.bind(on_press=answer_popup)
-        container.add_widget(btn_ok)
-        btn_cancel = Button(text="Добавить сумму", size_hint=(.3, .3),
-                            pos_hint={'x': .55, 'y': .1})
+        label_container.add_widget(lab)
+
+        button_container = BoxLayout(size_hint=(1, .3), padding=10, spacing=10)
+        btn_rewrite = Button(text="Переписать сумму", size_hint=(.3, 1), font_size=20)
+        btn_rewrite.bind(on_press=answer_popup)
+        btn_add_amount = Button(text="Добавить данные", size_hint=(.3, 1), font_size=23)
+        btn_add_amount.bind(on_press=answer_popup)
+        btn_cancel = Button(text="Удалить сумму", size_hint=(.3, 1), font_size=25)
         btn_cancel.bind(on_press=answer_popup)
-        container.add_widget(btn_cancel)
-        mynepopup.content = container
+
+        empty_container = BoxLayout(size_hint=(1, .3), padding=10, spacing=10)
+        btn_close_popup = Button(text="Закрыть", size_hint=(1, .5), font_size=23)
+        btn_close_popup.bind(on_press=mynepopup.dismiss)
+        empty_container.add_widget(btn_close_popup)
+
+        button_container.add_widget(btn_rewrite)
+        button_container.add_widget(btn_add_amount)
+        button_container.add_widget(btn_cancel)
+
+        root_container.add_widget(label_container)
+        root_container.add_widget(button_container)
+        root_container.add_widget(empty_container)
+
+        mynepopup.content = root_container
         mynepopup.open()  # Запустить Poput
 
     def get_key_for_dict(self):
         day = self.day_day_spinner.text
         month = self.month_month_spinner.text
         key = day + " " + month
-        self.KEY_DICT = key
+        #self.KEY_DICT = key
         return key
 
 
@@ -267,7 +324,7 @@ class Pages(Carousel):
         value = self.create_value_for_dict(rub,kop,comment)
 
         self.file_dict[key] = value
-        self.write_file_time_work()
+        self.write_file()
         print(self.file_dict,"Старый ключ с новым значением")
 
     @staticmethod
@@ -283,24 +340,21 @@ class Pages(Carousel):
             file_dict = {}
         return file_dict
 
-    def my_callback(self, instance):
+    def my_callback(self, time):
         self.label_save_txt = ""
-        return
 
-    def write_file_time_work(self):
-        with open("data_base.dat", 'wb') as obj:
-            pickle.dump(self.file_dict, obj)
-        Clock.schedule_once(self.my_callback, 2)
-        self.label_save_txt = "Сохранено"
-        key = self.get_key_for_dict()
-        self.update_statistic(key)
-        print(self.file_dict)
 
-    def install_date_spinner_day(self,choice_month:str):
-        """Установка кол-ва дней в спиннер в зависимости от месяца"""
-        indx_month = month_lst.index(choice_month)
-        quan_day = quantity_day[indx_month]
-        self.quantity_day_property = quan_day + 1
+    def write_file(self):
+        try:
+            with open("data_base.dat", 'wb') as obj:
+                pickle.dump(self.file_dict, obj)
+            Clock.schedule_once(self.my_callback, 2)
+            self.label_save_txt = "Данные успешно сохранены"
+            key = self.get_key_for_dict()
+            self.update_statistic(key)
+            print(self.file_dict)
+        except Exception as err:
+            self.label_save_txt = "Ошибка: ",err
 
     def focus_textinput(self, instance, value, text):
         if value and text == "0":
@@ -308,9 +362,18 @@ class Pages(Carousel):
         elif not value and text == "":
             instance.text = "0"
 
+
+
+    def install_date_spinner_day(self,choice_month:str):
+        """Установка кол-ва дней в спиннер в зависимости от месяца"""
+        indx_month = month_lst.index(choice_month)
+        quan_day = quantity_day[indx_month]
+        self.quantity_day_property = quan_day + 1
+
+
 class MyApp(App):
     def build(self):
-        Window.clearcolor = ("#40E0D0")
+        Window.clearcolor = "#40E0D0"
         obj = Pages()
         return obj
 
